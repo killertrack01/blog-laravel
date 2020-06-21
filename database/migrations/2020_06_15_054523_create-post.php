699c1fb0831b10db1391997504bf8cdb8fae66bb
killertrack01 <56512521+killertrack01@users.noreply.github.com>
2020-06-21 10:36:37 +0700

    migration update (role) by Thanh

---
+++ database/migrations/2020_06_15_054523_create-post.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePost extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('post', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->text('summary');
+            $table->longText('detail');
+            $table->string('img');
+            $table->integer('status');
+            $table->dateTime('created_at');
+            $table->dateTime('updated_at');
+            $table->integer('category_id')->unsigned();
+            $table->integer('users_id')->unsigned();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('post');
+    }
+}
---
+++ database/migrations/2020_06_15_054719_create-comment.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateComment extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comment', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('detail');
+            $table->dateTime('created_at');
+            $table->dateTime('updated_at');
+            $table->integer('post_id')->unsigned();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comment');
+    }
+}
